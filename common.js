020e21c3b809f6b208d99a60f5f049fba83ab8e1
Kristen Gerring <kgerring@gmail.com>
2025-04-05 14:45:56 -0400

    aa

---
+++ common.js
@@ -0,0 +1,131 @@
+// https://en.wikipedia.org/wiki/en:Special:MyPage/common.js
+
+// script to load recoin with only external ids @todo
+importScript('User:Vvekbv/recoin_id.js');
+mw.loader.load( '//www.wikidata.org/w/index.php?title=User%3ASo9q%2FScriptInstaller.js&action=raw&ctype=text%2Fjavascript' ); // 
+importScript('User:Abbe98/copy-qid.js');
+importScript('User:Fnielsen/scholia-link.js'); 
+mw.loader.load( '//www.wikidata.org/w/index.php?title=User%3AMagnus_Manske%2Fduplicate_item.js&action=raw&ctype=text%2Fjavascript' ); 
+mw.loader.load( '//www.wikidata.org/w/index.php?title=User%3ALockal%2FEditSum.js&action=raw&ctype=text%2Fjavascript' ); 
+mw.loader.load( '//www.wikidata.org/w/index.php?title=User%3AEfly%2FGadget-CreateNewItem.js&action=raw&ctype=text%2Fjavascript' ); 
+mw.loader.load( '//www.wikidata.org/w/index.php?title=User%3AFnielsen%2Fscholia-link.js&action=raw&ctype=text%2Fjavascript' ); 
+importScript('User:Wd-Ryan/EditCounterOptIn.js'); 
+importScript('User:Wd-Ryan/common.js'); 
+mw.loader.load( '//www.wikidata.org/w/index.php?title=User%3ATomT0m%2Fclassification.js&action=raw&ctype=text%2Fjavascript' ); 
+mw.loader.load('//www.wikidata.org/w/index.php?title=User%3ALectrician1%2Fmetaclass-check.js&action=raw&ctype=text%2Fjavascript' );
+mw.loader.load( '//www.wikidata.org/w/index.php?title=User%3AJon_Harald_S%C3%B8by%2FlinkItemsInDescriptions.js&action=raw&ctype=text%2Fjavascript' );
+mw.loader.load('//www.wikidata.org/w/index.php?title=User%3AYMS%2Flabelcollect.js&action=raw&ctype=text%2Fjavascript' );
+mw.loader.load('//www.wikidata.org/w/index.php?title=User%3AMichaelSchoenitzer%2Fquickpresets.js&action=raw&ctype=text%2Fjavascript' ); 
+mw.loader.load('//www.wikidata.org/w/index.php?title=User%3AData-Complete-Gadget%2FWikidataComplete.js&action=raw&ctype=text%2Fjavascript' ); 
+mw.loader.load('//www.wikidata.org/wiki/User:Luca.favorido/linkypop.js&action=raw&ctype=text/javascript');
+importScript('User:Nikki/TalkPageHeader.js'); //Linkback: [[User:Nikki/TalkPageHeader.js]]
+importScript('User:Pigsonthewing/vector.js'); //Linkback: [[User:Pigsonthewing/vector.js]] 
+importScript('User:TomT0m/LexToWiktionary.js'); //Linkback: [[User:TomT0m/LexToWiktionary.js]] 
+importScript('User:TomT0m/queries.js'); //Linkback: [[User:TomT0m/queries.js]]
+importScript('User:Pigsonthewing/common.js'); //Linkback: [[User:Pigsonthewing/common.js]] 
+importScript('User:Vvekbv/recoin id.js'); //Linkback: [[User:Vvekbv/recoin id.js]]
+importScript('User:Magnus Manske/wd edit.js'); //Linkback: [[User:Magnus Manske/wd edit.js]]]
+importScript('User:JonnyJD/consistency check.js'); //Linkback: [[User:JonnyJD/consistency check.js]] 
+mw.loader.load('//www.wikidata.org/w/index.php?title=User%3ATeester%2FDisplayColourSwatches.js&action=raw&ctype=text%2Fjavascript' ); 
+//Script to warn about conflicting superclasses on instance/subclass statements
+mw.loader.load('//www.wikidata.org/w/index.php?title=User:JesseW/conflicting_superclass_warnings.js&action=raw&ctype=text/javascript'); // [[User:JesseW/conflicting_superclass_warnings.js]]
+importScript('User:YMS/labelcollect2.js'); //Linkback: [[User:YMS/labelcollect2.js]] Added by Script installer
+importScript('User:TomT0m/classification/sandbox.js'); //Linkback: [[User:TomT0m/classification/sandbox.js]] Added by Script installer
+importScript('User:Lectrician1/metaclass-check.js'); //Linkback: [[User:Lectrician1/metaclass-check.js]] Added by Script installer
+importScript('User:Daniel Mietchen/wikidp.js'); //Linkback: [[User:Daniel Mietchen/wikidp.js]] Added by Script installer
+importScript('User:Wd-Ryan/quickpresets settings.js'); //Linkback: [[User:Wd-Ryan/quickpresets settings.js]] Added by Script installer
+importScript('User:Fnielsen/EntityShape.js'); //Linkback: [[User:Fnielsen/EntityShape.js]] Added by Script installer
+importScript('User:Wd-Ryan/extratools.js'); //Linkback: [[User:Wd-Ryan/extratools.js]] Added by Script installer
+importScript('User:Magnus Manske/authority control.js'); //Linkback: [[User:Magnus Manske/authority control.js]] Added by Script installer
+importScript('User:Magnus Manske/mixnmatch gadget.js'); //Linkback: [[User:Magnus Manske/mixnmatch gadget.js]] Added by Script installer
+importScript('User:TomT0m/conjugation.js'); //Linkback: [[User:TomT0m/conjugation.js]] Added by Script installer
+importScript('User:TomT0m/DatacompleteSandbox.js'); //Linkback: [[User:TomT0m/DatacompleteSandbox.js]] Added by Script installer
+importScript('User:Bamyers99/UnitsConverter.js'); //Linkback: [[User:Bamyers99/UnitsConverter.js]] Added by Script installer
+importScript('User:Epìdosis/MnM gadget.js'); //Linkback: [[User:Epìdosis/MnM gadget.js]] Added by Script installer
+importScript('User:LiberatorG/mixnmatch gadget.js'); //Linkback: [[User:LiberatorG/mixnmatch gadget.js]] Added by Script installer
+importScript('User:Husky/modelitem-lookup.js'); //Linkback: [[User:Husky/modelitem-lookup.js]] Added by Script installer
+importScript('User:Bamyers99/ClassSuggester.js'); //Linkback: [[User:Bamyers99/ClassSuggester.js]] Added by Script installer
+importScript('User:YMS/labelcollect.js'); //Linkback: [[User:YMS/labelcollect.js]] Added by Script installer
+importScript('User:EpochFail/ItemQuality.js'); //Linkback: [[User:EpochFail/ItemQuality.js]] Added by Script installer
+importScript('User:Yair rand/checksitelinks.js'); //Linkback: [[User:Yair rand/checksitelinks.js]] Added by Script installer
+importScript('User:Yair rand/WikidataInfo.js'); //Linkback: [[User:Yair rand/WikidataInfo.js]] Added by Script installer
+importScript('User:Yair rand/common.js'); //Linkback: [[User:Yair rand/common.js]] Added by Script installer
+importScript('User:TomT0m/classification.js'); //Linkback: [[User:TomT0m/classification.js]] Added by Script installer
+importScript('User:JesseW/conflicting superclass warnings.js'); //Linkback: [[User:JesseW/conflicting superclass warnings.js]] Added by Script installer
+importScript('User:So9q/merriam-webster.js'); //Linkback: [[User:So9q/merriam-webster.js]] Added by Script installer
+importScript('User:Magnus Manske/author sort.js'); //Linkback: [[User:Magnus Manske/author sort.js]] Added by Script installer
+importScript('User:Magnus Manske/missing props.js'); //Linkback: [[User:Magnus Manske/missing props.js]] Added by Script installer
+importScript('User:Ricordisamoa/WikidataTrust.js'); //Linkback: [[User:Ricordisamoa/WikidataTrust.js]] Added by Script installer
+importScript('User:Lucas Werkmeister/hyphenation-point.js'); //Linkback: [[User:Lucas Werkmeister/hyphenation-point.js]] Added by Script installer
+importScript('User:Magnus Manske/referee.js'); //Linkback: [[User:Magnus Manske/referee.js]] Added by Script installer
+importScript('User:Zvpunry/WikibaseEcho.js'); //Linkback: [[User:Zvpunry/WikibaseEcho.js]] Added by Script installer
+importScript('User:Mahir256/syndepgraph.js'); //Linkback: [[User:Mahir256/syndepgraph.js]] Added by Script installer
+importScript('User:Bargioni/WikiBridge.js'); //Linkback: [[User:Bargioni/WikiBridge.js]] Added by Script installer
+importScript('User:Aude/CiteTool.js'); //Linkback: [[User:Aude/CiteTool.js]] Added by Script installer
+importScript('User:Lectrician1/qualifier-constraint-usage.js'); //Linkback: [[User:Lectrician1/qualifier-constraint-usage.js]] Added by Script installer
+importScript('User:Yair rand/ShowTalkLabels.js'); //Linkback: [[User:Yair rand/ShowTalkLabels.js]] Added by Script installer
+importScript('User:Husky/ifff-viewer-link.js'); //Linkback: [[User:Husky/ifff-viewer-link.js]] Added by Script installer
+importScript('User:Data-Complete-Gadget/WikidataComplete.js'); //Linkback: [[User:Data-Complete-Gadget/WikidataComplete.js]] Added by Script installer
+importScript('User:Magnus Manske/consistency check.js'); //Linkback: [[User:Magnus Manske/consistency check.js]] Added by Script installer
+importScript('User:Pasleim/derivedstatements.js'); //Linkback: [[User:Pasleim/derivedstatements.js]] Added by Script installer
+importScript('User:Danmichaelo/Gadget-externalitemsuggester.js'); //Linkback: [[User:Danmichaelo/Gadget-externalitemsuggester.js]] Added by Script installer
+importScript('User:Bargioni/UseAsRef.js'); //Linkback: [[User:Bargioni/UseAsRef.js]] Added by Script installer
+importScript('User:Ricordisamoa/GeneaWiki.js'); //Linkback: [[User:Ricordisamoa/GeneaWiki.js]] Added by Script installer
+importScript('User:Tomayac/freebase2wikidata.js'); //Linkback: [[User:Tomayac/freebase2wikidata.js]] Added by Script installer
+importScript('User:Jon Harald Søby/linkItemsInDescriptions.js'); //Linkback: [[User:Jon Harald Søby/linkItemsInDescriptions.js]] Added by Script installer
+importScript('User:Magnus Manske/sourcerer.js'); //Linkback: [[User:Magnus Manske/sourcerer.js]] Added by Script installer
+importScript('User:Ricordisamoa/DMYdates.js'); //Linkback: [[User:Ricordisamoa/DMYdates.js]] Added by Script installer
+importScript('User:MichaelSchoenitzer/CiteTool.js'); //Linkback: [[User:MichaelSchoenitzer/CiteTool.js]] Added by Script installer
+importScript('User:Tpt/viaf.js'); //Linkback: [[User:Tpt/viaf.js]] Added by Script installer
+importScript('User:Nw520/QualiRef.js'); //Linkback: [[User:Nw520/QualiRef.js]] Added by Script installer
+importScript('User:Karima Rafes/LinkUniversity.js'); //Linkback: [[User:Karima Rafes/LinkUniversity.js]] Added by Script installer
+importScript('User:NavinoEvans/tours/scripts/wbadminterritory.js'); //Linkback: [[User:NavinoEvans/tours/scripts/wbadminterritory.js]] Added by Script installer
+importScript('User:NavinoEvans/tours/scripts/wbcoordinates.js'); //Linkback: [[User:NavinoEvans/tours/scripts/wbcoordinates.js]] Added by Script installer
+importScript('User:Ricordisamoa/SitenamesInUserLanguage.js'); //Linkback: [[User:Ricordisamoa/SitenamesInUserLanguage.js]] Added by Script installer
+importScript('User:Abbe98/osm.js'); //Linkback: [[User:Abbe98/osm.js]] Added by Script installer
+importScript('User:Nikki/KeyShortcuts.js'); //Linkback: [[User:Nikki/KeyShortcuts.js]] Added by Script installer
+importScript('User:Denny/articlePreview.js'); //Linkback: [[User:Denny/articlePreview.js]] Added by Script installer
+importScript('User:Nikki/ExpandReferences.js'); //Linkback: [[User:Nikki/ExpandReferences.js]] Added by Script installer
+importScript('User:Underlying lk/reasonator.js'); //Linkback: [[User:Underlying lk/reasonator.js]] Added by Script installer
+importScript('User:Ricordisamoa/ACimport.js'); //Linkback: [[User:Ricordisamoa/ACimport.js]] Added by Script installer
+importScript('User:FRacco/logoLinks.js'); //Linkback: [[User:FRacco/logoLinks.js]] Added by Script installer
+importScript('User:DannyS712/SortedPropertiesUpdater.js'); //Linkback: [[User:DannyS712/SortedPropertiesUpdater.js]] Added by Script installer
+importScript('User:Aude/citoid.js'); //Linkback: [[User:Aude/citoid.js]] Added by Script installer
+importScript('User:Ricordisamoa/PropertyNames.js'); //Linkback: [[User:Ricordisamoa/PropertyNames.js]] Added by Script installer
+importScript('User:Egon Willighagen/cdkdepict gadget.js'); //Linkback: [[User:Egon Willighagen/cdkdepict gadget.js]] Added by Script installer
+importScript('User:Vvekbv/recoin.js'); //Linkback: [[User:Vvekbv/recoin.js]] Added by Script installer
+importScript('User:NavinoEvans/tours/scripts/wbofficialwebsite.js'); //Linkback: [[User:NavinoEvans/tours/scripts/wbofficialwebsite.js]] Added by Script installer
+importScript('User:Yair rand/FindRedirectsForAliases.js'); //Linkback: [[User:Yair rand/FindRedirectsForAliases.js]] Added by Script installer
+importScript('User:Ricordisamoa/SetGender.js'); //Linkback: [[User:Ricordisamoa/SetGender.js]] Added by Script installer
+importScript('User:Joern/altLabels.js'); //Linkback: [[User:Joern/altLabels.js]] Added by Script installer
+importScript('User:Lectrician1/embeds.js'); //Linkback: [[User:Lectrician1/embeds.js]] Added by Script installer
+importScript('User:Shisma/wikidata2ical.js'); //Linkback: [[User:Shisma/wikidata2ical.js]] Added by Script installer
+importScript('User:Lectrician1/discographies.js'); //Linkback: [[User:Lectrician1/discographies.js]] Added by Script installer
+importScript('User:Nikki/Ranker.js'); //Linkback: [[User:Nikki/Ranker.js]] Added by Script installer
+importScript('User:Nikki/LowercaseLabels.js'); //Linkback: [[User:Nikki/LowercaseLabels.js]] Added by Script installer
+importScript('User:Zuphilip/wd edit.js'); //Linkback: [[User:Zuphilip/wd edit.js]] Added by Script installer
+importScript('User:Nikki/DisplayColourSwatches.js'); //Linkback: [[User:Nikki/DisplayColourSwatches.js]] Added by Script installer
+importScript('User:Nikki/Sidebar.js'); //Linkback: [[User:Nikki/Sidebar.js]] Added by Script installer
+importScript('User:So9q/wikidata-orcid-scraper-link.js'); //Linkback: [[User:So9q/wikidata-orcid-scraper-link.js]] Added by Script installer
+importScript('User:Nikki/Subpages.js'); //Linkback: [[User:Nikki/Subpages.js]] Added by Script installer
+importScript('User:Teester/CheckShex.js'); //Linkback: [[User:Teester/CheckShex.js]] Added by Script installer
+importScript('User:Nikki/Backlinks.js'); //Linkback: [[User:Nikki/Backlinks.js]] Added by Script installer
+importScript('User:Waldyrious/mainLanguageFirst.js'); //Linkback: [[User:Waldyrious/mainLanguageFirst.js]] Added by Script installer
+importScript('User:Magnus Manske/import statements.js'); //Linkback: [[User:Magnus Manske/import statements.js]] Added by Script installer
+importScript('User:SR5/Numeral converter.js'); //Linkback: [[User:SR5/Numeral converter.js]] Added by Script installer
+importScript('User:Bargioni/CC Lit Box.js'); //Linkback: [[User:Bargioni/CC Lit Box.js]] Added by Script installer
+importScript('User:Lockal/wikidata-link-reflector.js'); //Linkback: [[User:Lockal/wikidata-link-reflector.js]] Added by Script installer
+importScript('User:Lockal/Gadget-externalitemsuggester.js'); //Linkback: [[User:Lockal/Gadget-externalitemsuggester.js]] Added by Script installer
+importScript('User:Lectrician1/UserContributionsHeaderLink.js'); //Linkback: [[User:Lectrician1/UserContributionsHeaderLink.js]] Added by Script installer
+importScript('User:Lagewi/bibliography.js'); //Linkback: [[User:Lagewi/bibliography.js]] Added by Script installer
+importScript('User:Lagewi/properties.js'); //Linkback: [[User:Lagewi/properties.js]] Added by Script installer
+importScript('User:Lagewi/references.js'); //Linkback: [[User:Lagewi/references.js]] Added by Script installer
+importScript('User:So9q/wikidata-topic-curator-link.js'); //Linkback: [[User:So9q/wikidata-topic-curator-link.js]] Added by Script installer
+importScript('User:Bovlb/notability.js'); //Linkback: [[User:Bovlb/notability.js]] Added by Script installer
+importScript('User:Lagewi/navigation.js'); //Linkback: [[User:Lagewi/navigation.js]] Added by Script installer
+importScript('User:KDGerring/global.js'); //Linkback: [[User:KDGerring/global.js]] Added by Script installer
+importScript('User:Lectrician1/add-required-qualifier.js'); //Linkback: [[User:Lectrician1/add-required-qualifier.js]] Added by Script installer
+mw.loader.load('//www.wikidata.org/w/index.php?title=User:1Veertje/identifierInput.js&action=raw&ctype=text/javascript' ); // [[User:1Veertje/identifierInput.js]]
+importScript('User:Husky/thirdpartyformatters.js'); //Linkback: [[User:Husky/thirdpartyformatters.js]] Added by Script installer
+importScript('User:The-erinaceous-one/Gadget-relateditems.js'); //Linkback: [[User:The-erinaceous-one/Gadget-relateditems.js]] Added by Script installer
+importScript('User:Lockal/Gadget-relateditems.js'); //Linkback: [[User:Lockal/Gadget-relateditems.js]] Added by Script installer
