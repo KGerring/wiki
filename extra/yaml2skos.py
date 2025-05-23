82ceadbc11ac6296fc4bae7e3c01f05c921657c5
KGerring <KGerring@gmail.com>
2025-05-23 15:19:52 -0400

    Modified by www.SourceFiles.app

--- extra/yaml2skos.py
+++ extra/yaml2skos.py
@@ -334,6 +334,22 @@ def main():
         out_file.write(voc.serialize(format=args.output_format))
     print("Done")
 
+__all__ = sorted(
+    [
+        getattr(v, "__name__", k)
+        for k, v in list(globals().items())  # export
+        if (
+            (
+                callable(v)
+                and getattr(v, "__module__", "")
+                == __name__  # callables from this module
+                or k.isupper()
+            )
+            and not str(getattr(v, "__name__", k)).startswith("__")  # or CONSTANTS
+        )
+    ]
+)  # neither marked internal
+
 
 if __name__ == "__main__":
     main()
